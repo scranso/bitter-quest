7ac93a87ff7be444d98a9dd8f065750237858b1a
Sarah Ransom <sarahransom@MacBookPro.home.local>
2025-07-19 12:58:14 -0400

    added product information and routes

--- src/app/api/products/product_data.ts
+++ src/app/api/products/product_data.ts
@@ -1,16 +1,45 @@
 export const product_data = [
+
     {
         id: 1,
         name: "Green Chartreuse",
         description: "This lime-green, anise-scented liqueur is sweet at first sip, then herbaceous in the middle, showing fennel, pine, tarragon and mild floral notes. The finish is spicy and warming, with anise, white pepper and ginger heat. It's a bit too viscous and fiery for straight-up sipping, but it's beautifully complex and a key ingredient for the classic Last Word and other cocktails. - Wine Enthusiast",
-        price: 68.99,
-        type: "Amaro",
+        // price: 68.99,
+        type: "Liqueur",
 
-        "store_data": [
-            { 1:{
-                 availbilty: false, 
-                 count: 4
-                }
+        store_data: [
+            {   1:{
+                    availbilty: false, 
+                    
+                    },
+                2:{
+                    availbilty: false, 
+                    
+                   },
+                3:{
+                    availbilty: false, 
+                    
+                    },
+                4:{
+                    availbilty: false, 
+                    
+                    },
+                5:{
+                    availbilty: true, 
+                    price: 68.99
+                    },
+                6:{
+                    availbilty: false, 
+                    
+                    },
+                7:{
+                    availbilty: true, 
+                    price: 129.99,
+                    },
+                8:{
+                    availbilty: true, 
+                    price: 64.97
+                    },
             }
         ]
     },
@@ -19,32 +48,176 @@ export const product_data = [
         id: 2,
         name: "Amaro Di Angostura",
         description: "Colour, mahogany. Aroma, sweet spices, dried herbs, powerful, hot. Flavour, powerful, long, spicy. Color, caoba. Aroma, especias dulces, hierbas secas, potente, cálido. Boca, potente, largo, especiado. Farbe, mahagonibraun. Aroma, süße Gewürze, trockene Kräuter, kraftvoll, warm. Mund, kraftvoll, lang, würzig. - Guia Penin",
-        price: 26.99,
+        // price: 26.99,
         type: "Amaro",
+
+        store_data: [
+            {   1:{
+                    availbilty: false, 
+                    
+                    },
+                2:{
+                    availbilty: false, 
+                    
+                   },
+                3:{
+                    availbilty: false, 
+                    
+                    },
+                4:{
+                    availbilty: true, 
+                    price: 26.99
+                    },
+                5:{
+                    availbilty: false, 
+                    
+                    },
+                6:{
+                    availbilty: false, 
+                    
+                    },
+                7:{
+                    availbilty: false, 
+                    
+                    },
+                8:{
+                    availbilty: false, 
+                    
+                    },
+            }
+        ]
     },
 
     {
         id: 3,
         name: "Tempus Fugit Creme De Banane",
         description: "Indulgent and aromatic, reminiscent of brown bread, bananas foster, molasses, brown spice, and tobacco. Creamy and luscious, it offers intense brown sugar, chocolate-covered bananas, and a balanced sweetness, with a finish of malted chocolate and banana ice cream. - Finalist, Ultimate Beverage Challenge",
-        price: 45.99,
+        // price: 45.99,
         type: "Liqueur",
+
+        store_data: [
+            {   1:{
+                    availbilty: false, 
+                    
+                    },
+                2:{
+                    availbilty: true, 
+                    price: 45.99
+                   },
+                3:{
+                    availbilty: false, 
+                    
+                    },
+                4:{
+                    availbilty: false, 
+                    
+                    },
+                5:{
+                    availbilty: false, 
+                    
+                    },
+                6:{
+                    availbilty: true, 
+                    price: 42.99
+                    },
+                7:{
+                    availbilty: false, 
+                    
+                    },
+                8:{
+                    availbilty: false, 
+                    
+                    },
+            }
+        ]
     },
 
     {
         id: 4,
         name: "Carpano Antica Formula Sweet Vermouth",
         description: "Colour: light brown with an amber hue. – Nose: bouquet of vanilla with spicy, citrusy and dried fruit notes of almond, raisins and cloves. – Palate: well-rounded, rich vanilla taste that picks up the aromas from the nose. Combines well with the bitter orange, dates, cocoa beans and saffron flavours.(“Antica Formula”, www.carpano.com/",
-        price: 28.09,
+        // price: 28.09,
         type: "Vermouth",
+
+        store_data: [
+            {   1:{
+                    availbilty: true, 
+                    price: 28.09
+                    },
+                2:{
+                    availbilty: true, 
+                    price: 39.99
+                   },
+                3:{
+                    availbilty: true, 
+                    price: 39.99
+                    },
+                4:{
+                    availbilty: true, 
+                    price: 32.99
+                    },
+                5:{
+                    availbilty: true, 
+                    price: 33.99
+                    },
+                6:{
+                    availbilty: true, 
+                    price: 44.99
+                    },
+                7:{
+                    availbilty: true, 
+                    price: 34.99
+                    },
+                8:{
+                    availbilty: false, 
+                    
+                    },
+            }
+        ]
     },
 
     {
         id: 5,
         name: "Becherovka",
         description: "This cheery buttercup-yellow spirit evolves from sweet to bitter in seconds flat. At first, it has a sweet, honey-like cast, then ginger and other herbal notes develop and, finally, it finishes dry, with an amaro-like bitter note. Traditionally consumed neat, Becherovka also stands up well to dilution (tonic, club soda) and makes a particularly refreshing apéritif chilled with a squeeze of lime. - Wine Enthusiast",
-        price: 59.99,
+        // price: 59.99,
         type: "Liqueur",
+
+        store_data: [
+            {   1:{
+                    availbilty: true, 
+                    price: 29.99
+                    },
+                2:{
+                    availbilty: false, 
+                    
+                   },
+                3:{
+                    availbilty: false, 
+                    
+                    },
+                4:{
+                    availbilty: true, 
+                    price: 18.99
+                    },
+                5:{
+                    availbilty: false, 
+                    
+                    },
+                6:{
+                    availbilty: true, 
+                    price: 27.99
+                    },
+                7:{
+                    availbilty: false, 
+                    
+                    },
+                8:{
+                    availbilty: false, 
+                    
+                    },
+            }
+        ]
     },
 
 
---
+++ src/app/api/route.ts
@@ -0,0 +1,7 @@
+// import { comments } from "./data";
+
+// export async function GET() {
+//     return Response.json(comments);
+// }
+
+// Not sure what this is for yet but afraid to scrap it for now
